8305a3187713142944869e45b2e820030730a549
jmvelizf <jose.veliz@globant.com>
2022-03-27 18:04:55 -0500

    walking and flying done

--- index.js
+++ index.js
@@ -2,28 +2,42 @@ const vehicle = document.getElementById('vehicle')
 let positions = vehicle.getBoundingClientRect()
 
 const { x,y } = positions
+console.log('current positions', x, y)
 let _x = x, _y = y;
+let acceleration = 4 // walking
+// let acceleration = 2 // flying
 
-let mode = 'flying'
-vehicle.innerHTML = `I am a vehicle ${mode}`
+// Walking true by default
+let isWalking = false
+let isFlying = false
 
 document.onkeydown = (e) => {
-  
-  if(e.key== 'ArrowUp'){
-    _y-=1
+  if(e.key== '1'){
+    // flying
+    isFlying = true
+    isWalking = false
+
+    vehicle.style.transition = '6s' // takes 6 seconds to arrive
   }
-  else if(e.key == 'ArrowDown'){
-    _y+=1
+  else if(e.key == '2'){
+    // walking
+    isWalking = true
+    isFlying = false
+
+    vehicle.style.transition = '1s' // takes 2 seconds to arrive
   }
-  else if(e.key == 'ArrowLeft'){
-    _x-=1
-  }
-  else if(e.key == 'ArrowRight'){
-    _x+=1
-  }
-  
-  vehicle.style.setProperty('left', _x + 'px')
-  vehicle.style.setProperty('top', _y + 'px')
+
+  vehicle.innerHTML = `I am a vehicle ${!isFlying ? 'flying' : 'walking' }`
+}
+
+document.onmousedown = (e) => {
+  const x = e.x
+  const y = e.y
+
+  if(!isWalking && !isFlying) return
+
+  vehicle.style.setProperty('left', x + 'px')
+  vehicle.style.setProperty('top', y + 'px')
 }
 
 
